2acf0d587f5d1db8b58a182aceae3cafd8501bd8
Kevin Duong <kelvin.timsach@gmail.com>
2015-11-04 00:05:38 +0700

    Add action to button

--- Algorythm/Algorythm/ViewController.swift
+++ Algorythm/Algorythm/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    
+    @IBOutlet weak var aButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        aButton.setTitle("Press me!", forState: .Normal)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed() {
+        view.backgroundColor = UIColor.brownColor()
+    }
 
 }
 
