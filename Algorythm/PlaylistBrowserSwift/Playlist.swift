33ab6cc5ade371a13c7c6ac369ff743281ed3a2c
Kevin Duong <kelvin.timsach@gmail.com>
2015-11-06 11:19:53 +0700

    Finish the

---
+++ Algorythm/PlaylistBrowserSwift/Playlist.swift
@@ -0,0 +1,21 @@
+//
+//  Playlist.swift
+//  PlaylistBrowserSwift
+//
+//  Created by Pasan Premaratne on 1/12/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+    
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+    
+}
